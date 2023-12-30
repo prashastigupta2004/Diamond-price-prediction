6776a6481e3911173801b62b5cd992c89ceebb8c
prashastigupta2004 <prashastigupta2002@gmail.com>
2023-12-30 18:19:54 +0530

    final prediction

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,15 +10,18 @@ from src.exception import CustomException
 from src.logger import logging
 import sys,os
 from dataclasses import dataclass
-from src.utils import save_obj
+from src.utils import save_object
+
+## Data Transformation config
 
 @dataclass
-class DataTransformationConfig:
+class DataTransformationconfig:
     preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
+## Data Ingestionconfig class
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
+        self.data_transformation_config=DataTransformationconfig()
 
     def get_data_transformation_object(self):
         try:
@@ -66,11 +69,11 @@ class DataTransformation:
             logging.info('Exception occured in data transformation')
             raise CustomException(e,sys)
         
-    def initiate_data_transformation(self,train_data_path,test_data_path):
+    def initiate_data_transformation(self,train_path,test_path):
         
         try:
-            train_df=pd.read_csv(train_data_path)
-            test_df=pd.read_csv(test_data_path)
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info('read train & test data completed')
             logging.info(f'train dataframe head: \n{train_df.head().to_string()}')
@@ -80,26 +83,26 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformation_object()
 
-            target_column='price'
-            drop_column=[target_column,'id']
+            target_column_name='price'
+            drop_columns=[target_column_name,'id']
 
             # training data
-            input_feature_train_df=train_df.drop(columns=drop_column,axis=1)
-            target_feature_train_df=train_df[target_column]
+            input_feature_train_df=train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
             #test data
-            input_feature_test_df=train_df.drop(columns=drop_column,axis=1)
-            target_feature_test_df=train_df[target_column]
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
             #data transformation
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.fit_transform(input_feature_test_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
             test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
 
-            save_obj(
+            save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
             )
@@ -114,4 +117,5 @@ class DataTransformation:
 
         except Exception as e:
 
+            logging.info("Exception occured in the initiate_datatransformation")
             raise CustomException(e,sys)
